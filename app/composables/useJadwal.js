ce2d499ca90868f98603d7acac24437d19a77502
Djaroephon <djaroephon@gmail.com>
2025-09-19 16:29:06 +0700

    Add: Menginput MK Lain (Semester 3)

--- app/composables/useJadwal.js
+++ app/composables/useJadwal.js
@@ -9,7 +9,62 @@ const jadwal = ref([
     ruangan: "M Lt 3 ",
     sks: "3"
   },
-  
+  {
+    id: 2,
+    namaMk: "Basis Data",
+    waktu: "14:00-17:00/Selasa",
+    dosen: "M. Bayu Wibawa, S.Ko., MMSI.",
+    ruangan: "Lab Kom 1",
+    sks: "3"
+  },
+  {
+    id: 3,
+    namaMk: "Jaringan Komputer",
+    waktu: "08:00-12:00/Rabu",
+    dosen: "Rizka Albar, S.Kom.,MT",
+    ruangan: "Lab Kom 1",
+    sks: "4"
+  },
+  {
+    id: 4,
+    namaMk: "Pemrograman Berorientasi Objek",
+    waktu: "14:00-18:00/Rabu",
+    dosen: "Mahendar Dwi Payana, S.ST.,MT",
+    ruangan: "Lab Kom 1",
+    sks: "4"
+  },
+  {
+    id: 5,
+    namaMk: "Organisasi dan Arsitektur Komputer",
+    waktu: "14:00-16:00/Sabtu",
+    dosen: "Rizka Albar, S.Kom.,MT",
+    ruangan: "Lab Kom 1",
+    sks: "2"
+  },
+  {
+    id: 6,
+    namaMk: "Logika Matematika",
+    waktu: "10:00-13:00/Sabtu",
+    dosen: "Mahyus Ihsan, M.Si ",
+    ruangan: "Pustaka 2",
+    sks: "3"
+  },
+  {
+    id: 7,
+    namaMk: "Database Management System",
+    waktu: "14:00-17:00/Kamis",
+    dosen: "M. Bayu Wibawa, S.Kom.,MMSI ",
+    ruangan: "Lab Kom 1",
+    sks: "3"
+  },
+  {
+    id: 8,
+    namaMk: "Scenario dan Design Game",
+    waktu: "08:00-11:00/Senin",
+    dosen: "Desita Ria Yusian TB, S.ST.,MT",
+    ruangan: "Lab Kom 1",
+    sks: "3"
+  }
 ])
 
 export function useJadwal() {
